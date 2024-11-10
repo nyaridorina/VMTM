f1043cb007d37c049e2745d475b95d2b7638bade
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-10 20:34:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import os
 import tempfile
-from swear_detection import detect_swear_words_in_audio
+from swear_detection import detect_swear_words_in_audio, convert_audio
 
 app = Flask(__name__)
 
@@ -22,10 +22,18 @@ def upload_audio():
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio:
         audio_file.save(temp_audio.name)
         temp_audio_path = temp_audio.name
-    
+
     try:
-        # Detect swear words in the provided audio file
-        swear_word_detected = detect_swear_words_in_audio(temp_audio_path)
+        # Convert the audio to the required format
+        temp_converted_audio_path = temp_audio_path + '_converted.wav'
+        convert_audio(temp_audio_path, temp_converted_audio_path)
+
+        # Detect swear words in the converted audio file
+        swear_word_detected = detect_swear_words_in_audio(temp_converted_audio_path)
+
+        # Remove the converted audio file after processing
+        os.unlink(temp_converted_audio_path)
+
         if swear_word_detected:
             message = 'Szitokszó észlelve az audióban!'
         else:
@@ -36,4 +44,5 @@ def upload_audio():
     return jsonify({'message': message}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

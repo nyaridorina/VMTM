a61fa0664cede39bc5190d7ea2dacc66981055be
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-17 20:28:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Update package list and install system dependencies
-apt-get update && apt-get install -y portaudio19-dev mpg123
+apt-get update && apt-get install -y portaudio19-dev libportaudio2 libportaudiocpp0
+
+# Install mpg123 for audio playback
+apt-get install -y mpg123
 
 # Upgrade pip
 pip install --upgrade pip
